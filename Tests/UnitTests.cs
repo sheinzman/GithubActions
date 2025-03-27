a47cd5c57c79ae72866085d9d0ad75d680b6f803
sheinzman <144940216+sheinzman@users.noreply.github.com>
2025-03-27 09:58:35 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ public class Addition
 	public void Add_Valid_Patino()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
-		Assert.AreEqual(7, Program.Add("3", "2"));
+		Assert.AreEqual(5, Program.Add("3", "2"));
 		Assert.AreEqual(12, Program.Add("5", "7"));
 	}
 
@@ -120,7 +120,7 @@ public class Power
 	{
 		Assert.AreEqual(1, Program.Power("1", "2"));
 		Assert.AreEqual(9, Program.Power("3", "2"));
-		Assert.AreEqual(0.5, Program.Power("1", "-2"));
+		Assert.AreEqual(1, Program.Power("1", "-2"));
 	}
 
 	[TestMethod]
