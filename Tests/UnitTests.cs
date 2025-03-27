a3a979318975a3ae436a72cef73c74fe5ad415fb
sheinzman <144940216+sheinzman@users.noreply.github.com>
2025-03-27 09:53:34 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ public class Addition
 	public void Add_Valid_Patino()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
-		Assert.AreEqual(5, Program.Add("3", "2"));
+		Assert.AreEqual(7, Program.Add("3", "2"));
 		Assert.AreEqual(12, Program.Add("5", "7"));
 	}
 
@@ -27,3 +27,116 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
 }
+
+[TestClass]
+public class Subtraction
+{
+	[TestMethod]
+	public void Subtract_Valid_Patino()
+	{
+		Assert.AreEqual(-1, Program.Subtract("1", "2"));
+		Assert.AreEqual(1, Program.Subtract("3", "2"));
+		Assert.AreEqual(20, Program.Subtract("27", "7"));
+	}
+
+	[TestMethod]
+	public void Subtract_Invalid_Patino()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+	}
+
+	[TestMethod]
+	public void Subtract_Null_Patino()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+	}
+}
+
+[TestClass]
+public class Multiplication
+{
+	[TestMethod]
+	public void Multiply_Valid_Patino()
+	{
+		Assert.AreEqual(0, Program.Multiply("0", "2"));
+		Assert.AreEqual(-6, Program.Multiply("-3", "2"));
+		Assert.AreEqual(35, Program.Multiply("5", "7"));
+	}
+
+	[TestMethod]
+	public void Multiply_Invalid_Patino()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
+	}
+
+	[TestMethod]
+	public void Multiply_Null_Patino()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+	}
+}
+
+[TestClass]
+public class Division
+{
+	[TestMethod]
+	public void Divide_Valid_Patino()
+	{
+		Assert.AreEqual(5, Program.Divide("10", "2"));
+		Assert.AreEqual(-4, Program.Divide("-20", "5"));
+		Assert.AreEqual(0, Program.Divide("0", "10"));
+	}
+
+	[TestMethod]
+	public void Divide_Invalid_Patino()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
+	}
+
+	[TestMethod]
+	public void Divide_Null_Patino()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+	}
+}
+
+[TestClass]
+public class Power
+{
+	[TestMethod]
+	public void Power_Valid_Patino()
+	{
+		Assert.AreEqual(1, Program.Power("1", "2"));
+		Assert.AreEqual(9, Program.Power("3", "2"));
+		Assert.AreEqual(0.5, Program.Power("1", "-2"));
+	}
+
+	[TestMethod]
+	public void Power_Invalid_Patino()
+	{
+		Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
+	}
+
+	[TestMethod]
+	public void Power_Null_Patino()
+	{
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
+	}
+}
+
