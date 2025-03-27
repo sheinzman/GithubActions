cb0956452d14521e13257611b7390f6cc55e530e
sheinzman <144940216+sheinzman@users.noreply.github.com>
2025-03-27 09:56:50 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ public class Addition
 	public void Add_Valid_Patino()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
-		Assert.AreEqual(7, Program.Add("3", "2"));
+		Assert.AreEqual(5, Program.Add("3", "2"));
 		Assert.AreEqual(12, Program.Add("5", "7"));
 	}
 
